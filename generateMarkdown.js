4c5feb584d1cf241c47f3e7a54306ad4b12a9384
Ines Radic <inesradic12314@gmail.com>
2021-01-21 12:30:23 -0800

    add empty return for license

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -31,6 +31,9 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
+   if (!license) {
+     return '';
+   }
   return renderLicenseLink(license) + renderLicenseBadge(license);
 }
 
@@ -68,7 +71,7 @@ ${data.contributing}:
 ${data.tests}
 
 ## Questions
-* [${data.username}](${data.link})
+* Github Profile: [${data.username}](${data.link})
 
 ${data.info}:
 * ${data.email}
