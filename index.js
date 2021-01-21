aacc37d0dab4cdb94ed2b22e0ff81f257ede695e
Ines Radic <inesradic12314@gmail.com>
2021-01-20 20:13:32 -0800

    generate title, description, license

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 // TODO: Include packages needed for this application
-
+const generateMarkdown = require('./generateMarkdown');
+const inquirer = require('inquirer');
+const {writeFile} = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your title?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the description?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'what license would you like to select?',
+        choices: ['MIT', 'Apache', 'Mozilla']
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    writeFile(fileName, data, 'utf8', (err) => console.log('error in file', err));
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    const answers = await inquirer.prompt(questions);
+    console.log('answers are: ', answers);
+    const genMarkDown = generateMarkdown(answers);
+    writeToFile(answers.title + '_readme.md', genMarkDown);
+}
 
 // Function call to initialize app
 init();
---
+++ work_readme.md
@@ -0,0 +1,5 @@
+# work
+
+## Description: is it going to work
+
+## License: [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)![License: MIT](https://opensource.org/licenses/MIT)
